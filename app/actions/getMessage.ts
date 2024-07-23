ad25b8d1e486a587d5bc6e0b0c8fd37e09f66748
Hamza <Hamzabashir02001@gmail.com>
2024-07-23 20:55:21 +0500

    commit

--- app/actions/getMessage.ts
+++ app/actions/getMessage.ts
@@ -1,38 +1,38 @@
-import prisma from "@/app/lib/prismadb";
+// import prisma from "@/app/lib/prismadb";
 
-interface IParams {
-  authorId: string;
-}
+// interface IParams {
+//   authorId: string;
+// }
 
-export default async function getMessage(params: IParams) {
-  try {
-    const { authorId } = params;
+// export default async function getMessage(params: IParams) {
+//   try {
+//     const { authorId } = params;
 
-    // Get messages for the current user
-    const messages = await prisma.message.findMany({
-      where: {
-        OR: [
-          { userId: authorId },
-          { listingId: { in: await getListingIdsForUser(authorId) } },
-        ],
-      },
-      include: {
-        user: true,
-        listing: true,
-      },
-    });
+//     // Get messages for the current user
+//     const messages = await prisma.message.findMany({
+//       where: {
+//         OR: [
+//           { userId: authorId },
+//           { listingId: { in: await getListingIdsForUser(authorId) } },
+//         ],
+//       },
+//       include: {
+//         user: true,
+//         listing: true,
+//       },
+//     });
 
-    return messages;
-  } catch (error: any) {
-    throw new Error(error);
-  }
-}
+//     return messages;
+//   } catch (error: any) {
+//     throw new Error(error);
+//   }
+// }
 
-async function getListingIdsForUser(userId: string) {
-  const listings = await prisma.listing.findMany({
-    where: { userId },
-    select: { id: true },
-  });
+// async function getListingIdsForUser(userId: string) {
+//   const listings = await prisma.listing.findMany({
+//     where: { userId },
+//     select: { id: true },
+//   });
 
-  return listings.map((listing) => listing.id);
-}
+//   return listings.map((listing) => listing.id);
+// }
