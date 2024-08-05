4af32a3cb205e4b01acf054c278bb5c74c36ad2a
Hamza <Hamzabashir02001@gmail.com>
2024-08-05 20:44:20 +0500

    virtual Tour name added

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -185,7 +185,7 @@ const ListingInfo: FC<ListingInfoProps> = ({
             rel="noopener noreferrer"
             className="inline-block py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
           >
-            View Panorama
+            View Virtual Tour
           </a>
         </div>
       )}
